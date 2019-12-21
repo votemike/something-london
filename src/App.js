65e50f5e90464500d134dc252c9fa2c0514fb8a3
Michael Gwynne <michael@votemike.co.uk>
2019-12-21 09:58:42 +0000

    Adding button and blurb

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showResults: false
+    };
+    const shortBlurb = (<div className="blurb">Can't decide what to do on your outing to London? Want to show a friend the quirkiest places in the city? Let us tell you.</div>);
+    const longBlurb = (<div className="blurb">
+      <div>Having a night out in London and want to find somewhere <span>fun</span>? Somewhere <span>different</span>? Somewhere <span>unique</span>? Somewhere "<span>London</span>"?</div>
+      <div>Don't fret or spend hours researching, let us help you out with somewhere fun to play, somewhere delicious to eat and somewhere unique to drink.</div>
+    </div>);
+    this.blurb = Math.random() >= 0.5 ? shortBlurb : longBlurb;
+    this.drink = this.chooseDrink();
+    this.eat = this.chooseEat();
+    this.play = this.choosePlay();
+  }
 
-  const plays = [
-      { name: 'Bounce', href: 'https://www.bouncepingpong.com/' },
-      { name: 'Flight Club', href: 'https://flightclubdarts.com/london/' },
-      { name: 'The Four Quarters', href: 'http://geocities.fourquartersbar.co.uk/' },
-      { name: 'London Shuffle', href: 'https://www.londonshuffle.com/' },
-      { name: 'Queens', href: 'https://queens.london/' },
-      { name: 'Swingers', href: 'https://swingersldn.com/home/' },
-      { name: 'Whistle Punks', href: 'https://www.whistlepunks.com/' }
-  ];
-  const eats = [
-      { name: 'Bel Canto', href: 'https://www.belcantolondon.co.uk/' },
-      { name: 'Bustronome', href: 'https://www.bustronome.com/en/london/' },
-      { name: 'Dans Le Noir', href: 'https://london.danslenoir.com/en/home/' },
-      { name: 'Hawksmoor', href: 'https://thehawksmoor.com/' },
-      { name: 'The Clink', href: 'https://theclinkcharity.org/restaurants/brixton' },
-      { name: 'The Jam', href: 'https://www.thejamrestaurant.com/' }
-  ];
-  const drinks = [
+  chooseDrink() {
+    const drinks = [
       { name: 'ABQ', href: 'https://abqlondon.com/abq-london' },
       { name: 'Ballie Ballerson', href: 'https://www.ballieballerson.com/' },
       { name: 'Bobby Fitzpatrick', href: 'https://www.bobbyf.co.uk/' },
@@ -29,37 +28,73 @@ function App() {
       { name: 'Little Nan\'s Bar', href: 'https://www.littlenans.co.uk/' },
       { name: 'Sky Garden', href: 'https://skygarden.london/' },
       { name: 'WC', href: 'http://www.wcclapham.co.uk/' }
-  ];
-  const play = random(plays);
-  const eat = random(eats);
-  const drink = random(drinks);
+    ];
 
-  return (
-    <div className="App">
-      <header className="App-header">
-          <div className="row">
-              <div className="column">
-                  <div>
-                    <h2>play</h2>
-                  </div>
-                  <a target="_blank" rel="noopener noreferrer" href={play.href} className="selection">{play.name}</a>
-              </div>
-              <div className="column">
-                  <div>
-                      <h2>eat</h2>
-                  </div>
-                  <a target="_blank" rel="noopener noreferrer" href={eat.href} className="selection">{eat.name}</a>
-              </div>
-              <div className="column">
-                  <div>
-                      <h2>drink</h2>
-                  </div>
-                  <a target="_blank" rel="noopener noreferrer" href={drink.href} className="selection">{drink.name}</a>
-              </div>
+    return random(drinks);
+  }
+
+  chooseEat() {
+    const eats = [
+      { name: 'Bel Canto', href: 'https://www.belcantolondon.co.uk/' },
+      { name: 'Bustronome', href: 'https://www.bustronome.com/en/london/' },
+      { name: 'Dans Le Noir', href: 'https://london.danslenoir.com/en/home/' },
+      { name: 'Hawksmoor', href: 'https://thehawksmoor.com/' },
+      { name: 'The Clink', href: 'https://theclinkcharity.org/restaurants/brixton' },
+      { name: 'The Jam', href: 'https://www.thejamrestaurant.com/' }
+    ];
+
+    return random(eats);
+  }
+
+  choosePlay() {
+    const plays = [
+      { name: 'Bounce', href: 'https://www.bouncepingpong.com/' },
+      { name: 'Flight Club', href: 'https://flightclubdarts.com/london/' },
+      { name: 'The Four Quarters', href: 'http://geocities.fourquartersbar.co.uk/' },
+      { name: 'London Shuffle', href: 'https://www.londonshuffle.com/' },
+      { name: 'Queens', href: 'https://queens.london/' },
+      { name: 'Swingers', href: 'https://swingersldn.com/home/' },
+      { name: 'Whistle Punks', href: 'https://www.whistlepunks.com/' }
+    ];
+
+    return random(plays);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className={this.state.showResults ? 'hidden row first' : 'row first'}>
+            {this.blurb}
+            <button onClick={() => {
+              this.setState({ showResults: true })
+            }}>Choose!
+            </button>
           </div>
-      </header>
-    </div>
-  );
+          <div className={this.state.showResults ? 'row second' : 'hidden row second'}>
+            <div className="column">
+              <div>
+                <h2>play</h2>
+              </div>
+              <a target="_blank" rel="noopener noreferrer" href={this.play.href} className="selection">{this.play.name}</a>
+            </div>
+            <div className="column">
+              <div>
+                <h2>eat</h2>
+              </div>
+              <a target="_blank" rel="noopener noreferrer" href={this.eat.href} className="selection">{this.eat.name}</a>
+            </div>
+            <div className="column">
+              <div>
+                <h2>drink</h2>
+              </div>
+              <a target="_blank" rel="noopener noreferrer" href={this.drink.href} className="selection">{this.drink.name}</a>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 function random(list) {
